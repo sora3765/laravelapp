21c7d5cf3bf79b85b855216ecf4889fee42a16c5
SO <hnksb640@yahoo.co.jp>
2021-10-25 16:49:20 +0900

    HelloControllerの作成

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    //add
+    public function index()
+    {
+        return 'Hello World';
+    }
+}
